a1c2c7a0ce73df35d03d19d853195fae4f90de5c
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-08-01 16:57:53 -0400

    got square generation started

---
+++ js-file.js
@@ -0,0 +1,16 @@
+let selectedGird =0;
+function createGird(grid){
+let gridNum = grid * grid;
+for(var i = 0; i< gridNum; i++){
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = '';
+content.style.color = 'blue';
+content.style.border = 'solid';
+content.style.width = "10px"
+content.style.height = "10px"
+container.appendChild(content);
+}
+}
+createGird(5);
